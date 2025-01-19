039213c34206022cb9fd8542c4d972612bc18959
Florian Rieger <mail@riegerflo.com>
2025-01-19 21:30:36 +0100

    Make package installable by adding setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""Python setup file."""
+from setuptools import setup
+
+setup(
+    name='citadels',
+    version='0.1',
+    packages=['citadels'],
+    install_requires=[
+    ],
+    entry_points={
+        'console_scripts': [
+            'citadels = citadels.__main__:main',
+        ],
+    },
+)
